4f6556a3df2e31d02eba204ad3110ff4974bde37
Balakrishnan <mscbalakrishnan@gmail.com>
2018-03-20 23:17:52 +0530

    String related code

---
+++ IntellPython/src/DemoString/StringDelete.py
@@ -0,0 +1,5 @@
+my_string = 'programiz'
+my_string[5] = 'a'
+
+my_string = 'python'
+print(my_string)
---
+++ IntellPython/src/DemoString/StringManipulation.py
@@ -0,0 +1,20 @@
+count = 0
+for letter in 'Hello World':
+    if(letter == 'l'):
+        count += 1
+print(count,'letters found')
+
+# default(implicit) order
+default_order = "{}, {} and {}".format('John','Bill','Sean')
+print('\n--- Default Order ---')
+print(default_order)
+
+# order using positional argument
+positional_order = "{1}, {0} and {2}".format('John','Bill','Sean')
+print('\n--- Positional Order ---')
+print(positional_order)
+
+# order using keyword argument
+keyword_order = "{s}, {b} and {j}".format(j='John',b='Bill',s='Sean')
+print('\n--- Keyword Order ---')
+print(keyword_order)
